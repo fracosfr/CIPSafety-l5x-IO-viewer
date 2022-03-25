1346455861ad502b80f387bb9dc0a2dbd7a0bf0f
fc-nov <92509225+fc-nov@users.noreply.github.com>
2022-03-25 11:06:16 +0100

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,3 +1,10 @@
 import os
 
-os.system("pyinstaller main.py -F -w -i icon.ico -n CIPSafety-l5x-IO-viewer")
+upx_dir = 'C:\\upx'
+param_upx = ""
+
+
+if os.path.exists(upx_dir):
+    param_upx = f"--upx-dir \"{upx_dir}\""
+    
+os.system(f"pyinstaller main.py -F -w -i icon.ico -n CIPSafety-l5x-IO-viewer -y {param_upx}")
